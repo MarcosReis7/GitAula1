fe02b4b2063bb2f28c0ab4e9b33f4b4b4c1c149a
MarcosReis7 <mreis6357133@gmail.com>
2021-08-24 16:57:02 -0300

    Adicionando nova variável e operação de multiplicação

--- src/ExemploSoma.java
+++ src/ExemploSoma.java
@@ -1,9 +1,13 @@
 
 public class ExemploSoma {
 	public static void main (String args[]) {
-		int num1 = 10;
-		int num2 = 15;
+		int num1 = 7;
+		int num2 = 5;
+		int num3 = 10;
 		
-		System.out.println(num1 + num2);
+		
+		System.out.println("O resultado da soma é:"+ num1 + num2);
+		System.out.println("O resultado da multiplicação é:"+ num1 + num3);
+		
 	}
 }
